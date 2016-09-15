93c12fc27323e2b262b6ff4fc109c37465a3baa2
Hazrat Ali <iloveyii@yahoo.com>
2016-09-15 07:18:19 +0200

    Added message_body

--- http.erl
+++ http.erl
@@ -12,3 +12,29 @@ request_line([$G, $E, $T, 32 | R0]) ->
 	{Ver, R2} = http_version(R1),
 	[13,10|R3] = R2,
 	{{get, URI, Ver}, R3}. 
+
+request_uri([32|R0]) ->
+	{[], R0};
+request_uri([C|R0]) ->
+	{Rest, R1} = request_uri(R0),
+	{[C|Rest], R1}.
+
+http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
+	{v11, R0};
+http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
+	{v10, R0}.
+
+headers([13,10|R0]) ->
+	{[],R0};
+headers(R0) ->
+	{Header, R1} = header(R0),
+	{Rest, R2} = headers(R1),
+	{[Header|Rest], R2}.
+header([13,10|R0]) ->
+	{[], R0};
+header([C|R0]) ->
+	{Rest, R1} = header(R0),
+	{[C|Rest], R1}.
+
+message_body(R) ->
+{R, []}.
