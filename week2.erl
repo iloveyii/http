7369768ed26f52ecb01165a2522c42198c841987
Hazrat Ali <iloveyii@yahoo.com>
2016-09-29 22:31:19 +0200

    Added sum of list - direct recursion

--- week2.erl
+++ week2.erl
@@ -1,7 +1,29 @@
 -module(week2).
--export([second/0]).
+-export([second1/0, second2/1, second3/1, head/1, tail/1, sum/1 ]).
 
-second() ->
+% List example - list is a collection of similar elements %
+second1() ->
     A = [2,3,4,5],
     hd(tl(A)).
 
+head(B) ->
+    [X|_Xs] = B,
+    X.
+
+tail(C) ->
+    [_X|Xs] = C,
+    Xs.
+
+second2(D) ->
+    [_X|Xs] = D,
+    head(Xs).
+
+second3(E) ->
+    [_X,Y|_Zs] = E,
+    Y.
+
+% Find sum of a list - direct recursion %
+sum([]) -> 0;
+sum(F) ->
+    [X|Xs] = F,
+    X + sum(Xs).
